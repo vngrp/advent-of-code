ffefd69fb4d29dc90b1a993025cf4cf9f3b5005f
Pim van Gurp <gurp@duck.com>
2023-12-04 22:18:27 +0100

    Make day 4 shorter

--- src/aoc2023/Day04.kt
+++ src/aoc2023/Day04.kt
@@ -6,34 +6,24 @@ import java.io.File
 
 fun main() = Day4().solve(13, 30)
 
-typealias Card = Pair<Set<Int>, Set<Int>>
-
-class Day4 : Day<List<Card>>(4, 2023) {
+class Day4 : Day<List<List<Int>>>(4, 2023) {
     override fun parse(file: File) = file
         .readLines()
         .map { line ->
-            val winning = line.substringAfter(": ").substringBefore(" |").split(" ").mapNotNull { it.toIntOrNull() }
-            val owned = line.substringAfter(": ").substringAfter("| ").split(" ").mapNotNull { it.toIntOrNull() }
+            val (_, winning, owned) = line.split(":", "|")
 
-            winning.toSet() to owned.toSet()
+            winning.split(" ").intersect(owned.split(" ").toSet()).mapNotNull { it.toIntOrNull() }
         }
 
-    override fun part1(input: List<Card>) = input.sumOf {
-        if (it.winningNumbers().isEmpty()) {
-            0
-        } else {
-            it.winningNumbers().drop(1).fold(1L) { acc, _ -> acc * 2 }
-        }
+    override fun part1(input: List<List<Int>>) = input.sumOf {
+        if (it.isEmpty()) 0
+        else 1.shl(it.size - 1)
     }
     
-    override fun part2(input: List<Card>) = input
-        .foldIndexed(input.indices.associateWith { 1 }) { cardIndex, acc, card ->
-            card
-                .winningNumbers()
-                .foldIndexed(acc) { winningNumberIndex, copies, _  ->
-                    copies.put(cardIndex + winningNumberIndex + 1) { (it ?: 0) + getOrDefault(cardIndex, 0) }
-                }
+    override fun part2(input: List<List<Int>>) = input
+        .foldIndexed(input.indices.associateWith { 1 }) { cardIndex, acc, winningNumbers ->
+            winningNumbers.foldIndexed(acc) { winningNumberIndex, copies, _  ->
+                copies.put(cardIndex + winningNumberIndex + 1) { (it ?: 0) + getOrDefault(cardIndex, 0) }
+            }
         }.values.sum()
-
-    private fun Card.winningNumbers() = first.intersect(second)
 }
